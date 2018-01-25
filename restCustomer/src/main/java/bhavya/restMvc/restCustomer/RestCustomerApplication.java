535a5ce234d008cb60472f19306f76bece65d838
unknown <bhavarora17@gmail.com>
2018-01-25 09:38:36 -0700

    Initial Commit

---
+++ restCustomer/src/main/java/bhavya/restMvc/restCustomer/RestCustomerApplication.java
@@ -0,0 +1,12 @@
+package bhavya.restMvc.restCustomer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RestCustomerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RestCustomerApplication.class, args);
+	}
+}
